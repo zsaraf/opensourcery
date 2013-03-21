81024b0feff452e6f426703b68a19cac2861250f
Paul Julius Martinez <Paul@dn0a208b46.sunet>
2013-03-20 21:22:30 -0700

    No clue what's happenning.

--- web.js
+++ web.js
@@ -2,47 +2,47 @@ var http = require('http')
 var fs   = require('fs');
 var path = require('path');
 
-// var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 
-// http.createServer(function(request, response) {
+http.createServer(function(request, response) {
 
-// 	var filePath = '.' + request.url;
-// 	if (filePath == './') filePath = './index.html';
+	var filePath = '.' + request.url;
+	if (filePath == './') filePath = './index.html';
 
-// 	var extension = path.extname(filePath);
-// 	var contentType = '';
-// 	switch (extension) {
-// 		case '.html': contentType = 'text/html'; break;
-// 		case '.js': contentType = 'text/javascript'; break;
-// 		case '.css': contentType = 'text/css'; break;
-// 	}
+	var extension = path.extname(filePath);
+	var contentType = 'text/html';
+	switch (extension) {
+		case '.html': contentType = 'text/html'; break;
+		case '.js': contentType = 'text/javascript'; break;
+		case '.css': contentType = 'text/css'; break;
+	}
 
-// 	fs.readFile(filePath, function(err, content) {
-// 		if (err) {
-// 			throw err;
-// 		}
-// 		response.writeHeader(200, {'Content-Type' : contentType});
-// 		response.write(content);
-// 		response.end();
-// 	});
-
-// }).listen(port, function(){
-// 	console.log("Listening on " + port);
-// });
-
-
-
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }    
-    var port = process.env.PORT || 5000;   
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port, function(){
-	console.log("Listening on " + port);
+	fs.readFile(filePath, function(err, content) {
+		if (err) {
+			throw err;
+		}
+		response.writeHead(200, {'Content-Type' : contentType});
+		response.write(content);
+		response.end();
 	});
+
+}).listen(port, function(){
+	console.log("Listening on " + port);
 });
+
+
+
+
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }    
+//     var port = process.env.PORT || 5000;   
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(port, function(){
+// 	console.log("Listening on " + port);
+// 	});
+// });
