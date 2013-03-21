0cda105b790f85eeeba92da3371673c5e312aa85
Calvin Studebaker <calstud@stanford.edu>
2013-03-20 19:24:00 -0700

    please work

--- web.js
+++ web.js
@@ -1,26 +1,16 @@
-// var http = require('http');
-
-// var app = http.createServer(http.logger());
-
-// app.get('/', function(request, response) {
-//   response.send('Hello World, this is Opensourcery!');
-// });
-
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
-
 var http = require('http'),
     fs = require('fs');
 
 fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
-    }       
+    }    
+    var port = process.env.PORT || 5000;   
     http.createServer(function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(8000);
+    }).listen(port, function(){
+    	console.log("Listening on " + port);
+    });
 });
